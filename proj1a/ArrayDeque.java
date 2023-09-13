bcedaf9b17663da4858c3978ece2e4559fab9e96
R-y-a-n-S <114883517+R-y-a-n-S@users.noreply.github.com>
2023-09-13 01:47:50 -0700

    Add files via upload

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,211 @@
+public class ArrayDeque<T> {
+    public T[] baseArr;
+    private int size;
+    private int front;
+    private int back;
+    private boolean firstFront = true;
+    private boolean firstBack = true;
+    private boolean not_resized = true;
+    public ArrayDeque() {
+        baseArr = (T[]) new Object[8];
+        size = 0;
+        front = 0;
+        back = baseArr.length - 1;
+    }
+    public void addFirst(T item) {
+        //resize
+        if (size == baseArr.length) {
+            T[] newArr = (T[]) new Object[baseArr.length * 2];
+            int ind = 0;
+            int tempSize = size;
+            int tempback = back;
+            if (front == tempback && front == baseArr.length - 1) {
+                tempback = getPev(tempback);
+            }
+            while (tempSize > 0) {
+                if (baseArr[tempback] != null) {
+                    newArr[ind] = baseArr[tempback];
+                    tempback = getPev(tempback);
+                    ind ++;
+                    tempSize --;
+                }
+            }
+            baseArr = newArr;
+            front = size - 1;
+            back = 0;
+            not_resized = false;
+        }
+
+        if (firstFront && not_resized) {
+            firstFront = false;
+        } else {
+            if (front == baseArr.length - 1) front = 0;
+            else front++;
+
+        }
+        baseArr[front] = item;
+        size ++;
+    }
+
+    public void addLast(T item) {
+        if (size == baseArr.length) {
+            T[] newArr = (T[]) new Object[baseArr.length * 2];;
+            int ind = 0;
+            int tempSize = size;
+            int tempback = back;
+            if (front == tempback && front == baseArr.length - 1) {
+                tempback = getPev(tempback);
+            }
+            while (tempSize > 0) {
+                if (baseArr[tempback] != null) {
+                    newArr[ind] = baseArr[tempback];
+                    tempback = getPev(tempback);
+                    ind ++;
+                    tempSize --;
+                }
+            }
+
+            baseArr = newArr;
+            front = size - 1;
+            back = 0;
+            not_resized = false;
+        }
+        if (firstBack && not_resized) {
+            firstBack = false;
+        } else {
+            if (back == 0) back = baseArr.length - 1;
+            else back --;
+        }
+        baseArr[back] = item;
+        size ++;
+    }
+    private int getNext (int currentFront) {
+        if (currentFront == 0) return baseArr.length - 1;
+        return --currentFront;
+    }
+
+    private int getPev (int currentBack) {
+        if (currentBack == baseArr.length - 1) return 0;
+        return ++currentBack;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+
+        int tempSize = size;
+        int tempfront = front;
+        if (back == tempfront && back == 0) {
+            tempfront = getNext(tempfront);
+        }
+        while (tempSize > 0) {
+            if (baseArr[tempfront] != null) {
+                System.out.print(baseArr[tempfront]);
+                tempfront = getNext(tempfront);
+                tempSize --;
+                if (tempSize == 0) {
+                    break;
+                }
+                System.out.print(" ");
+            }else {
+                tempfront = getNext(tempfront);
+            }
+        }
+    }
+
+    public T removeFirst() {
+        if (size == 0) return null;
+        T out;
+
+        if (size == 1) {
+            if (baseArr[front] != null) {
+                out = baseArr[front];
+                baseArr[front] = null;
+            }
+            else {
+                out = baseArr[back];
+                baseArr[back] = null;
+            }
+            front = 0;
+            back = baseArr.length - 1;
+            size --;
+            firstBack = true;
+            firstFront = true;
+            not_resized = true;
+            return out;
+        }
+
+        out = baseArr[front];
+        baseArr[front] = null;
+        size --;
+        if (front == 0) {
+            front = baseArr.length - 1;
+        } else {
+            front --;
+        }
+        return out;
+    }
+
+    public T removeLast() {
+        if (size == 0) return null;
+        T out;
+
+        if (size == 1) {
+            if (baseArr[front] != null) {
+                out = baseArr[front];
+                baseArr[front] = null;
+            }
+            else {
+                out = baseArr[back];
+                baseArr[back] = null;
+            }
+            front = 0;
+            back = baseArr.length - 1;
+            size --;
+            firstBack = true;
+            firstFront = true;
+            not_resized = true;
+            return out;
+        }
+
+        out = baseArr[back];
+        baseArr[back] = null;
+        if (back == baseArr.length - 1) {
+            back = 0;
+        } else {
+            back ++;
+        }
+        size --;
+        return out;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null; // Index out of bounds
+        }
+        int tempSize = size;
+        int tempfront = front;
+        if (back == tempfront && back == 0) {
+            tempfront = getNext(tempfront);
+        }
+        while (index > 0) {
+            if (baseArr[tempfront] != null) {
+                tempfront = getNext(tempfront);
+                index --;
+
+            }else {
+                tempfront = getNext(tempfront);
+            }
+        }
+        return baseArr[tempfront];
+
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,145 @@
+public class LinkedListDeque<T> {
+    private DLL baseLL;
+    public LinkedListDeque() {
+        baseLL = new DLL();
+    }
+    public void addFirst(T item) {
+        baseLL.addFirst(item);
+    }
+
+    public void addLast(T item) {
+        baseLL.addLast(item);
+    }
+
+    public boolean isEmpty() {
+        return baseLL.size == 0;
+    }
+
+    public int size() {
+        return baseLL.size;
+    }
+
+    public void printDeque() {
+        Node temp = baseLL.front;
+        while (temp.next != null) {
+
+            System.out.print(temp.next.item);
+            if (temp.next == baseLL.last) break;
+            System.out.print(" ");
+            temp = temp.next;
+        }
+
+    }
+
+    public T removeFirst() {
+        return baseLL.removeFirst();
+    }
+
+    public T removeLast() {
+        return baseLL.removeLast();
+    }
+
+    public T get(int index) {
+        if (index >= baseLL.size || index < 0) return null;
+        Node temp = baseLL.front;
+        for ( int i = 0; i <= index; i ++) {
+            temp = temp.next;
+            if (i == index) return (T) temp.item;
+        }
+        return null;
+
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= baseLL.size) {
+            return null; // Index out of bounds
+        }
+        return (T) getRecursive2(index, baseLL.front.next);
+    }
+
+    private T getRecursive2(int index, Node<T> current) {
+        if (index == 0) {
+            return current.item;
+        }
+        return (T) getRecursive2(index - 1, current.next);
+    }
+
+    public class Node<T> {
+        public T item;
+        public Node next;
+        public Node prev;
+
+
+        public Node(T i, Node n, Node p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    public class DLL {
+        public Node front;
+        public Node last;
+        public int size;
+        public DLL() {
+            front = new Node<>(null, last, null);
+            last = new Node<>(null, null, front);
+            front = new Node<>(null, last, null);
+            size = 0;
+        }
+        public void addFirst(T data) {
+            Node newNode = new Node<>(data,front.next,front);
+            front.next.prev = newNode;
+            front.next = newNode;
+            size ++;
+        }
+
+        public void addLast(T data) {
+            Node newNode = new Node<>(data,last,last.prev);
+            last.prev.next = newNode;
+            last.prev = newNode;
+            size ++;
+        }
+
+        public T removeFirst() {
+            if (size == 0) return null;
+            T item = (T) front.next.item;
+            Node temp = front.next.next;
+            temp.prev = front;
+            front.next.next = null;
+            front.next.prev = null;
+            front.next = temp;
+            size --;
+            return item;
+        }
+
+        public T removeLast() {
+            if (size == 0) return null;
+            T item = (T) last.prev.item;
+            Node temp = last.prev.prev;
+            temp.next = last;
+            last.prev.prev = null;
+            last.prev.next = null;
+            last.prev = temp;
+            size --;
+            return item;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
